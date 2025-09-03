83d13e17f1bcddd9a45291f791d63752e4d39bc2
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-03 11:02:48 +0530

    MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const connectDB = async () => {
+  const connect = await mongoose.connect(process.env.MONGODB_URL);
+  if (connect) {
+    console.log("\n📶 MongoDB connected");
+  }
+};
 
 app.get("/health", (req, res) => {
   res.json({ status: "OK", message: "Server is healthy" });
@@ -14,5 +22,6 @@ app.get("/health", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+  console.log(`\n📞 Server is listening on ${PORT}`);
+  connectDB();
 });
