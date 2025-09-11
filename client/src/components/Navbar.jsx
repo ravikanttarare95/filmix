3d6e97729d7a1f59a359d02f5644a4dba81b7810
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-11 14:47:31 +0530

    Modified Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,22 +7,9 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-violet-950 text-white shadow-md sticky w-full z-50">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        <img src={Logo} alt="Filmix" className="w-20 h-auto mr-4" />
-
-        <div className="hidden sm:flex space-x-8">
-          {NAV_LINKS.map((navLinkObj, index) => (
-            <a
-              key={index}
-              href={navLinkObj.to}
-              className="text-white hover:text-violet-300 text-lg transition-colors duration-300 font-medium"
-            >
-              {navLinkObj.name}
-            </a>
-          ))}
-        </div>
-
+    <nav className="p-2 flex flex-col sm:flex-row justify-around sm:items-center bg-violet-950 text-white shadow-md sticky w-full z-50">
+      <div className="flex justify-between items-center">
+        <img src={Logo} alt="Filmix" className="w-20" />
         <div className="sm:hidden">
           <button
             onClick={() => setIsOpen(!isOpen)}
@@ -30,26 +17,23 @@ function Navbar() {
           >
             {isOpen ? <X size={30} /> : <Menu size={30} />}
           </button>
-        </div>
+        </div>{" "}
       </div>
-
       <div
-        className={`sm:hidden bg-violet-950 transition-all duration-300 ${
-          isOpen ? "max-h-96 py-4" : "max-h-0 overflow-hidden"
-        }`}
+        className={` ${
+          isOpen ? "flex flex-col" : "hidden"
+        } sm:flex sm:flex-row space-y-5 sm:space-x-8`}
       >
-        <div className="flex flex-col items-center space-y-4">
-          {NAV_LINKS.map((navLinkObj, index) => (
-            <a
-              key={index}
-              href={navLinkObj.to}
-              className="text-white hover:text-violet-300 text-lg transition-colors duration-300 font-medium"
-              onClick={() => setIsOpen(false)}
-            >
-              {navLinkObj.name}
-            </a>
-          ))}
-        </div>
+        {NAV_LINKS.map((navLinkObj, index) => (
+          <a
+            key={index}
+            href={navLinkObj.to}
+            className="text-white hover:text-violet-300 text-lg text-center transition-colors duration-300 font-medium"
+            onClick={() => setIsOpen(false)}
+          >
+            {navLinkObj.name}
+          </a>
+        ))}
       </div>
     </nav>
   );
