7be73e3b71d05e9e2799198e32f2bc200eff271d
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-13 12:42:11 +0530

    Form Error Handling

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Input({ type, name, id, placeholder, value, onInputChange }) {
+function Input({ type, name, id, placeholder,min,max, value, onInputChange }) {
   return (
     <input
       type={type}
       name={name}
       id={id}
       placeholder={placeholder}
+      min={min}
+      max={max}
       className="shadow p-3"
       value={value}
       onChange={onInputChange}
--- client/src/views/AddMovies.jsx
+++ client/src/views/AddMovies.jsx
@@ -18,19 +18,45 @@ function AddMovies() {
     releaseYear: "",
     rating: 0,
   });
-  const [newImages, setNewImages] = useState([]);
-  const [error, setError] = useState({
-    title: "",
-    description: "",
-    images: "",
-    category: "",
-    director: "",
-    releaseYear: "",
-    rating: "",
-  });
+  const [newImages, setNewImages] = useState("");
+  const [errorTitle, setErrorTitle] = useState("");
+  const [errorDescription, setErrorDescription] = useState("");
+  const [errorImages, setErrorImages] = useState("");
+  const [errorCategory, setErrorCategory] = useState("");
+  const [errorDirector, setErrorDirector] = useState("");
+  const [errorReleaseYear, setErrorReleaseYear] = useState("");
+  const [errorRating, setErrorRating] = useState("");
 
-  const handleAddMovie = async (e) => {
+  const handleAddMovie = async () => {
     try {
+      if (
+        movieDetail.title.trim() === "" ||
+        movieDetail.description.trim() === "" ||
+        movieDetail.category.trim() === "" ||
+        movieDetail.director.trim() === "" ||
+        movieDetail.releaseYear.trim() === "" ||
+        movieDetail.rating === ""
+      ) {
+        return toast.error("Please fill all the fields");
+      }
+
+      if (movieDetail.images.length === 0) {
+        setErrorImages("Please add at least one image URL");
+        return toast.error("Please add at least one image URL");
+      } else {
+        setErrorImages("");
+      }
+      if (
+        errorTitle ||
+        errorDescription ||
+        errorImages ||
+        errorCategory ||
+        errorDirector ||
+        errorReleaseYear ||
+        errorRating
+      ) {
+        return toast.error("Please fix the errors before submitting");
+      }
       const response = await axios.post(
         `${import.meta.env.VITE_API_URL}/movies`,
         movieDetail
@@ -39,33 +65,74 @@ function AddMovies() {
       setTimeout(() => {
         navigate("/movies");
       }, 1000);
-    } catch (error) {}
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
     if (!movieDetail.title) {
-      setError({ ...error, title: "" });
+      setErrorTitle("");
     } else if (movieDetail.title.length < 3) {
-      setError({
-        ...error,
-        title: "Movie title must be at least 2 characters",
-      });
+      setErrorTitle("Movie title must be at least 2 characters");
+    } else {
+      setErrorTitle("");
     }
 
     if (!movieDetail.description) {
-      setError({ ...error, description: "" });
+      setErrorDescription("");
     } else if (movieDetail.description.length < 10) {
-      setError({
-        ...error,
-        description: "Description must be at least 10 characters",
-      });
+      setErrorDescription("Description must be at least 10 characters");
+    } else {
+      setErrorDescription("");
+    }
+
+    if (!movieDetail.category) {
+      setErrorCategory("");
+    } else if (movieDetail.category.length < 3) {
+      setErrorCategory("Category must be at least 3 characters");
+    } else {
+      setErrorCategory("");
+    }
+    if (!movieDetail.director) {
+      setErrorDirector("");
+    } else if (movieDetail.director.length < 3) {
+      setErrorDirector("Director name must be at least 3 characters");
+    } else {
+      setErrorDirector("");
+    }
+    const releaseYearNum = Number(movieDetail.releaseYear);
+    if (!releaseYearNum) {
+      setErrorReleaseYear("");
+    } else if (
+      isNaN(releaseYearNum) ||
+      releaseYearNum < 1888 ||
+      releaseYearNum > new Date().getFullYear()
+    ) {
+      setErrorReleaseYear("Please enter a valid release year");
+    } else {
+      setErrorReleaseYear("");
+    }
+    const ratingNum = Number(movieDetail.rating);
+    if (ratingNum === "") {
+      setErrorRating("");
+    } else if (isNaN(ratingNum) || ratingNum < 0 || ratingNum > 5) {
+      setErrorRating("Rating must be a number between 0 and 5");
+    } else {
+      setErrorRating("");
     }
   }, [movieDetail]);
 
   return (
     <div className="max-w-150 mx-auto">
       {" "}
-      <form onSubmit={handleAddMovie} className="flex flex-col gap-5">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddMovie();
+        }}
+        className="flex flex-col gap-5"
+      >
         <Input
           type="text"
           name="movie-title"
@@ -76,7 +143,7 @@ function AddMovies() {
             setMovieDetail({ ...movieDetail, title: e.target.value });
           }}
         />
-        {error.title && <p className="text-red-500 text-sm">{error.title}</p>}
+        {errorTitle && <p className="text-red-500 text-sm">{errorTitle}</p>}
 
         <textarea
           name="movie-desc"
@@ -88,8 +155,8 @@ function AddMovies() {
             setMovieDetail({ ...movieDetail, description: e.target.value });
           }}
         ></textarea>
-        {error.description && (
-          <p className="text-red-500 text-sm">{error.description}</p>
+        {errorDescription && (
+          <p className="text-red-500 text-sm">{errorDescription}</p>
         )}
 
         {movieDetail.images.length !== 0 && (
@@ -131,14 +198,20 @@ function AddMovies() {
             setNewImages(e.target.value);
           }}
         />
+        {errorImages && <p className="text-red-500 text-sm">{errorImages}</p>}
 
         <MdAddPhotoAlternate
           onClick={() => {
+            if (newImages.length === 0) {
+              return toast.error("Please add proper image URL");
+            }
+
             setMovieDetail({
               ...movieDetail,
               images: [...movieDetail.images, newImages],
             });
-            setNewImages([]);
+            setNewImages("");
+            setErrorImages("");
           }}
           className="cursor-pointer"
         />
@@ -153,6 +226,9 @@ function AddMovies() {
             setMovieDetail({ ...movieDetail, category: e.target.value });
           }}
         />
+        {errorCategory && (
+          <p className="text-red-500 text-sm">{errorCategory}</p>
+        )}
 
         <Input
           type="text"
@@ -164,35 +240,50 @@ function AddMovies() {
             setMovieDetail({ ...movieDetail, director: e.target.value });
           }}
         />
-        <Input
-          type="number"
-          id="release-year"
-          name="release-year"
-          placeholder=""
-          value={movieDetail?.releaseYear}
-          onInputChange={(e) => {
-            setMovieDetail({ ...movieDetail, releaseYear: e.target.value });
-          }}
-        />
-        <Input
-          type="number"
-          id="rating"
-          name="rating"
-          placeholder="Movie Rating"
-          value={movieDetail?.rating}
-          onInputChange={(e) => {
-            setMovieDetail({ ...movieDetail, rating: e.target.value });
-          }}
-        />
+        {errorDirector && (
+          <p className="text-red-500 text-sm">{errorDirector}</p>
+        )}
+        <div className="flex gap-5">
+          <div className="flex flex-col w-full">
+            <p>Realese Year:</p>
+            <Input
+              type="number"
+              id="release-year"
+              name="release-year"
+              placeholder=""
+              value={movieDetail?.releaseYear}
+              min={1888}
+              max={new Date().getFullYear()}
+              onInputChange={(e) => {
+                setMovieDetail({ ...movieDetail, releaseYear: e.target.value });
+              }}
+            />
+            {errorReleaseYear && (
+              <p className="text-red-500 text-sm">{errorReleaseYear}</p>
+            )}
+          </div>
+          <div className="flex flex-col w-full">
+            <p>Ratings:</p>
+            <Input
+              type="number"
+              id="rating"
+              name="rating"
+              min={0}
+              max={5}
+              placeholder="Movie Rating"
+              value={movieDetail?.rating}
+              onInputChange={(e) => {
+                setMovieDetail({ ...movieDetail, rating: e.target.value });
+              }}
+            />
 
-        <Button
-          type="submit"
-          btnTitle=" Add Movie"
-          onBtnClick={(e) => {
-            e.preventDefault();
-            handleAddMovie();
-          }}
-        />
+            {errorRating && (
+              <p className="text-red-500 text-sm">{errorRating}</p>
+            )}
+          </div>
+        </div>
+
+        <Button type="submit" btnTitle=" Add Movie" />
       </form>
       <Toaster position="top-right" />
     </div>
